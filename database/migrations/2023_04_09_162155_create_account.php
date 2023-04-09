95f16b3d1cb90998b96ecd03328d8640d80239f1
fourki <fourki2002@gmail.com>
2023-04-10 00:48:24 +0700

    table

--- database/migrations/2023_04_09_162155_create_account.php
+++ database/migrations/2023_04_09_162155_create_account.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('account', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
