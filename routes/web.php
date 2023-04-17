3e9f9f6cf9914afaa2fda974f5caa079454015a5
fourki <fourki2002@gmail.com>
2023-04-17 11:46:06 +0700

    menambahkan dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin/dashboard_admin');
 });
 
 
